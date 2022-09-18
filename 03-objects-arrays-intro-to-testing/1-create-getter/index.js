f18ea1a7615a132ab460cf663bcbcadb0c5de0b3
Andrey Zhegalov <zhegalovan@gmail.com>
2022-09-18 10:49:39 +0300

    Added implementation createGetter()

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const sep = '.';
+  const [...parts] = path.split(sep);
+  return function (obj) {
+    let curObject = obj;
+    for (const part of parts) {
+      if (curObject === undefined) {
+        break;
+      }
+      curObject = curObject[part];
+    }
+    return curObject;
+  };
 }
